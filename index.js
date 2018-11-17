e2a6bbfb0998dada1da3c9b13b99fbde9222607f
Martin Kolárik <martin@kolarik.sk>
2018-11-18 00:24:26 +0100

    Use median instead of average, improve regex

--- index.js
+++
@@ -1,6 +0,0 @@
-const COMMENT_PATTERN = /\/\*[\s\S]*?\*\/|\/\/.*?(?:\n|$)/g;
-
-module.exports = function (code) {
-	code = code.replace(COMMENT_PATTERN, '');
-	return code.length / code.split('\n').length > 200;
-};
